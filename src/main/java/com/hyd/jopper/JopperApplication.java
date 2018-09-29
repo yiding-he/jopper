7ad5b2c543259f2ec5e05f619c46dd2e3e29bcba
HeYiding <yiding.he@gmail.com>
2018-09-29 23:40:40 +0800

    init

---
+++ src/main/java/com/hyd/jopper/JopperApplication.java
@@ -0,0 +1,12 @@
+package com.hyd.jopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JopperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JopperApplication.class, args);
+    }
+}
